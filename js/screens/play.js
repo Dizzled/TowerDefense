21aa1a1b99658fb5b3f472b841b7e479b36556af
Dustin LaGrone <dlagrone@corp.aerovelco.com>
2019-02-02 16:51:33 -0800

    Updated Map

--- js/screens/play.js
+++ js/screens/play.js
@@ -5,7 +5,7 @@ game.PlayScreen = me.ScreenObject.extend({
     onResetEvent: function() {
         // reset the score
         game.data.score = 0;
-        me.levelDirector.loadLevel("Testarea3");
+        me.levelDirector.loadLevel("Tiledmap");
         // Add our HUD to the game world, add it last so that this is on top of the rest.
         // Can also be forced by specifying a "Infinity" z value to the addChild function.
         this.HUD = new game.HUD.Container();
