9c7518bace55ef544029d4639e6d4300e2de6763
dwkrueger2 <dwkrueger2@users.noreply.github.com>
2019-02-04 23:39:07 -0500

    Commiting kruegerd Branch

---
+++ js/resources.js
@@ -0,0 +1,62 @@
+game.resources = [
+    {
+        "name": "magecity",
+        "type": "image",
+        "src": "data/img/magecity.png"
+    },
+    {
+        "name": "mountain_landscape",
+        "type": "image",
+        "src": "data/img/mountain_landscape.png"
+    },
+    {
+        "name": "snowWIP",
+        "type": "image",
+        "src": "data/img/snowWIP.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/title_screen.png"
+    },
+    {
+        "name": "BigPreviewEight",
+        "type": "image",
+        "src": "./img/sprite/BigPreviewEight.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "./img/sprite/gripe_run_right.png"
+    },
+    {
+        "name": "Small_tower",
+        "type": "image",
+        "src": "./img/sprite/Small_tower.png"
+    },
+    {
+        "name": "spinning_coin_gold",
+        "type": "image",
+        "src": "./img/sprite/spinning_coin_gold.png"
+    },
+    {
+        "name": "wheelie_right",
+        "type": "image",
+        "src": "./img/sprite/wheelie_right.png"
+    },
+    {
+        "name": "Mountain_Snow",
+        "type": "tsx",
+        "src": "data/map/Mountain_Snow.tsx"
+    },
+    {
+        "name": "Zelda",
+        "type": "tsx",
+        "src": "data/map/Zelda.tsx"
+    },
+    {
+        "name": "Tiledmap",
+        "type": "tmx",
+        "src": "data/map/Tiledmap.tmx"
+    }
+];
